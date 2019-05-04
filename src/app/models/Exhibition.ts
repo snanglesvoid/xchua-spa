14cb4711738d5318ed1e73031fb31f2f8a24e457
Janik Hotz <snangles.void@gmail.com>
2019-05-04 16:33:02 +0200

    some more models;

--- src/app/models/Exhibition.ts
+++ src/app/models/Exhibition.ts
@@ -1,11 +1,17 @@
 import { Model } from './Model'
+import { CloudinaryImageModel, CloudinaryImage } from './CloudinaryImage'
+import { GallerySpaceModel, GallerySpace } from './GallerySpace';
+import { ArtistModel, Artist } from './Artist';
+import { ArtworkModel, Artwork } from './Artwork';
+import { ApiService } from '../services/api.service';
+import { Language } from '../services/language.service';
 
 export interface ExhibitionModel {
     _id : string
     slug: string
     title: { english: string, german?: string, chinese?: string }
     date: { start: Date, end: Date },
-    text: { english?: string, german?: string, chinese? string },
+    text: { english?: string, german?: string, chinese?: string },
     location: GallerySpaceModel | string
     artists: (ArtistModel | string)[]
     coverPicture?: CloudinaryImageModel
@@ -15,4 +21,69 @@ export interface ExhibitionModel {
 
 export class Exhibition extends Model {
 
+    constructor(api: ApiService, private model: ExhibitionModel) {
+        super(api)
+
+        if (typeof model.location === 'string') {
+            this._location = model.location
+        }
+        else {
+            this._location = new GallerySpace(api, model.location as GallerySpaceModel)
+        }
+        if (model.artists && model.artists.length && typeof model.artists[0] === 'string') {
+            this._artists = model.artists as string[]
+        }
+        else {
+            this._artists = model.artists.map(x => new Artist(api, x as ArtistModel))
+        }
+        if (model.artworks && model.artworks.length &&  typeof model.artworks === 'string') {
+            this._artworks = model.artworks as string[]
+        }
+        else {
+            this._artworks = model.artworks.map(x => new Artwork(api, x as ArtworkModel))
+        }
+
+        this._coverPicture = new CloudinaryImage(api, model.coverPicture)
+        this._pictures = model.pictures && model.pictures.length ? model.pictures.map(p => new CloudinaryImage(api, p)) : []
+    }
+
+    public get id () { return this.model._id }
+    public get slug() { return this.model.slug }
+    private _title: string
+    public get title() { return this._title }
+    private _start: Date
+    private _end: Date
+    public get start() { return this._start }
+    public get end() { return this._end }
+    private _text: string
+    public get text() { return this._text }
+    private _artists: (Artist | string)[]
+    public get artists() { return this._artists }
+    private _location: string | GallerySpace
+    public get location() { return this._location }
+    private _coverPicture: CloudinaryImage
+    public get coverPicture() { return this._coverPicture }
+    private _pictures: CloudinaryImage[]
+    public get pictures() { return this._pictures }
+    private _artworks: (Artwork | string)[]
+    public get artworks() { return this._artworks }
+
+    public translate(language: Language) {
+        this._title = this.model.title[language] || this.model.title.english
+        this._text = this.model.text[language] || this.model.text.english || ''
+
+        if (this.location instanceof GallerySpace) {
+            this.location.translate(language)
+        }
+        if (this.artists && this.artists.length && this.artists[0] instanceof Artist) {
+            this.artists.forEach((x:Artist) => x.translate(language))
+        }
+        if (this.artworks && this.artworks.length && this.artworks[0] instanceof Artwork) {
+            this.artworks.forEach((x: Artwork) => x.translate(language))
+        }
+    }
+
+    public populate(field: string) {
+        return Promise.reject('Not implemented')
+    }
 }
---
+++ src/app/models/GallerySpace.ts
@@ -0,0 +1,40 @@
+import { ApiService } from '../services/api.service';
+import { Language } from '../services/language.service';
+import { Model } from './Model';
+import { CloudinaryImageModel, CloudinaryImage } from './CloudinaryImage';
+
+export interface GallerySpaceModel {
+    _id: string
+    location: { english: string, german?: string, chinese?: string },
+    address: { english?: string, german?: string, chinese?: string },
+    phone?: string,
+    openingHours: { english?: string, german?: string, chinese?: string },
+    image: CloudinaryImageModel
+}
+
+export class GallerySpace extends Model {
+    constructor(api: ApiService, private model: GallerySpaceModel) {
+        super(api)
+        this._image = new CloudinaryImage(api, model.image)
+    }
+
+    public get id() {
+        return this.model._id
+    }
+    public get phone() { return this.model.phone }
+    private _image: CloudinaryImage
+    public get image() { return this._image }
+    private _location: string
+    public get location() { return this._location }
+    private _address: string
+    public get address() { return this._address }
+    private _openingHours: string
+    public get openingHours() { return this._openingHours }
+
+    translate(language: Language) {
+        this._location = this.model.location[language] || this.model.location.english
+        this._address = this.model.address[language] || this.model.address.english || ''
+        this._openingHours = this.model.openingHours[language] || this.model.openingHours.english || ''
+    }
+      
+}
